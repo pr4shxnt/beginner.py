3a182925980ecb2ee371d3093bb6d1943434e0ac
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-05-20 21:17:24 +0545

    loops and iteration: hangmanGame.py added

--- Introductory Programs/hangmanGame.py
+++ Introductory Programs/hangmanGame.py
@@ -9,6 +9,8 @@ for i in range(length):
         print(f"You got it right in {i + 1} tries!")
         break
     else:
-        print( hangman[:i+1])  # Show first i+1 letters as progress
+        print( hangman[:i+1])
 else:
     print(f"You didn't get it. Game over! The correct word was '{random_word}'.")
+
+
