d5a49a960d969e7c01bf0e5ca11448c8f0d1fa3c
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-05-20 21:21:40 +0545

    loops and iteration: hangmanGame.py added v2.0.1 message: while loop addded

--- Introductory Programs/hangmanGame.py
+++ Introductory Programs/hangmanGame.py
@@ -2,6 +2,8 @@ random_word = "GLASS"
 hangman = "HANGMAN"
 length = len(hangman)
 
+print("for loop")
+
 for i in range(length):
     guess = input("Enter your guess: ").upper()
     
@@ -14,3 +16,22 @@ else:
     print(f"You didn't get it. Game over! The correct word was '{random_word}'.")
 
 
+
+# using while loop now
+
+print("While loop")
+
+i = 0
+
+while length:
+    guess = input("Enter your guess: ").upper()
+
+    if guess == random_word:
+         print(f"You got it right in {i + 1} tries!")
+         break
+    elif guess != random_word:
+        print( hangman[:i+1])
+        i += 1
+    else:
+        print(f"You didn't get it. Game over! The correct word was '{random_word}'.")
+        break
